7ba766a012799d7c1b7163a505be8a2f794469c9
Dana Varahi <dana_varahi@protonmail.com>
2020-10-12 19:47:43 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,10 @@
+guess = input("What is my age?")
+
+if int(guess) == 35:
+    print("That's right!")
+
+elif int(guess) < 35:
+    print("I'm older than that")
+
+else:
+    print("I'm younger than that")
